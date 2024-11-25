15b83810d7d08b94ca9ea076d5cfce77712c2d11
unknown <Cetecom@SJAPCA03LC0604.duoc.acad>
2024-11-25 16:08:09 -0300

    agregado android y configurado cap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB80FE2hRSYQCEpbdUZFqgZVfzSYQhJf7U",
+    authDomain: "petstore-1bae6.firebaseapp.com",
+    projectId: "petstore-1bae6",
+    storageBucket: "petstore-1bae6.firebasestorage.app",
+    messagingSenderId: "979015061215",
+    appId: "1:979015061215:web:2979ebdbe53b9d5a8b236a",
+    measurementId: "G-PGK0Y013Y3",
+    }
 };
